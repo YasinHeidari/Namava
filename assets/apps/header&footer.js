a5a84d8a3346190f4b1fcae6e1ff9fe925ae91c3
YasinHeidari <yasin.gh.heidari@gmail.com>
2024-01-05 22:19:57 +0330

    added some slider movie

--- assets/apps/header&footer.js
+++ assets/apps/header&footer.js
@@ -1,12 +1,9 @@
-let sideMenuOpen = document.querySelector(".sideMenuOpen");
-let sideMenuClose = document.querySelector(".sideMenuClose");
-let hamburgerMenu = document.querySelector(".hamburgerMenu");
-let sideMenu = document.querySelector(".sideMenu");
+const sideMenu = document.querySelector(".sideMenu");
 
-hamburgerMenu.addEventListener("click",function sideMenuCollapse(){
-    if (sideMenu.style.right = '-50%'){
+function sideMenuCollapse(){
+    if (sideMenu.style.right === '-150%'){
         sideMenu.style.right = '0';
     }else {
-        sideMenu.style.right = '-50%';
+        sideMenu.style.right = '-150%';
     }
-})
+}
---
+++ assets/apps/sliderMovie.js
@@ -0,0 +1,221 @@
+ let movies1= {
+    "data": [{
+    "id": 1,
+    "title": "The Shawshank Redemption",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0111161_poster.jpg",
+    "year": "1994",
+    "country": "USA",
+    "imdb_rating": "9.3",
+    "genres": ["Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0111161_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0111161_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0111161_screenshot3.jpg"]
+}, {
+    "id": 2,
+    "title": "The Godfather",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0068646_poster.jpg",
+    "year": "1972",
+    "country": "USA",
+    "imdb_rating": "9.2",
+    "genres": ["Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0068646_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0068646_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0068646_screenshot3.jpg"]
+}, {
+    "id": 3,
+    "title": "The Godfather: Part II",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0071562_poster.jpg",
+    "year": "1974",
+    "country": "USA",
+    "imdb_rating": "9.0",
+    "genres": ["Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0071562_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0071562_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0071562_screenshot3.jpg"]
+}, {
+    "id": 4,
+    "title": "The Dark Knight",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0468569_poster.jpg",
+    "year": "2008",
+    "country": "USA, UK",
+    "imdb_rating": "9.0",
+    "genres": ["Action", "Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0468569_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0468569_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0468569_screenshot3.jpg"]
+}, {
+    "id": 5,
+    "title": "12 Angry Men",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0050083_poster.jpg",
+    "year": "1957",
+    "country": "USA",
+    "imdb_rating": "8.9",
+    "genres": ["Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0050083_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0050083_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0050083_screenshot3.jpg"]
+}, {
+    "id": 6,
+    "title": "Schindler's List",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0108052_poster.jpg",
+    "year": "1993",
+    "country": "USA",
+    "imdb_rating": "8.9",
+    "genres": ["Biography", "Drama", "History"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0108052_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0108052_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0108052_screenshot3.jpg"]
+}, {
+    "id": 7,
+    "title": "Pulp Fiction",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0110912_poster.jpg",
+    "year": "1994",
+    "country": "USA",
+    "imdb_rating": "8.9",
+    "genres": ["Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0110912_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0110912_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0110912_screenshot3.jpg"]
+}, {
+    "id": 8,
+    "title": "The Lord of the Rings: The Return of the King",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0167260_poster.jpg",
+    "year": "2003",
+    "country": "USA, New Zealand",
+    "imdb_rating": "8.9",
+    "genres": ["Adventure", "Drama", "Fantasy"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0167260_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0167260_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0167260_screenshot3.jpg"]
+}, {
+    "id": 9,
+    "title": "The Good, the Bad and the Ugly",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0060196_poster.jpg",
+    "year": "1966",
+    "country": "Italy, Spain, West Germany, USA",
+    "imdb_rating": "8.9",
+    "genres": ["Western"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0060196_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0060196_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0060196_screenshot3.jpg"]
+}, {
+    "id": 10,
+    "title": "Fight Club",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0137523_poster.jpg",
+    "year": "1999",
+    "country": "USA, Germany",
+    "imdb_rating": "8.8",
+    "genres": ["Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0137523_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0137523_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0137523_screenshot3.jpg"]
+}],
+    "metadata": {
+    "current_page": "1",
+    "per_page": 10,
+    "page_count": 25,
+    "total_count": 250
+}
+};
+    let movies2 = {
+    "data": [{
+    "id": 1,
+    "title": "The Shawshank Redemption",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0111161_poster.jpg",
+    "year": "1994",
+    "country": "USA",
+    "imdb_rating": "9.3",
+    "genres": ["Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0111161_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0111161_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0111161_screenshot3.jpg"]
+}, {
+    "id": 2,
+    "title": "The Godfather",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0068646_poster.jpg",
+    "year": "1972",
+    "country": "USA",
+    "imdb_rating": "9.2",
+    "genres": ["Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0068646_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0068646_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0068646_screenshot3.jpg"]
+}, {
+    "id": 3,
+    "title": "The Godfather: Part II",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0071562_poster.jpg",
+    "year": "1974",
+    "country": "USA",
+    "imdb_rating": "9.0",
+    "genres": ["Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0071562_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0071562_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0071562_screenshot3.jpg"]
+}, {
+    "id": 4,
+    "title": "The Dark Knight",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0468569_poster.jpg",
+    "year": "2008",
+    "country": "USA, UK",
+    "imdb_rating": "9.0",
+    "genres": ["Action", "Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0468569_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0468569_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0468569_screenshot3.jpg"]
+}, {
+    "id": 5,
+    "title": "12 Angry Men",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0050083_poster.jpg",
+    "year": "1957",
+    "country": "USA",
+    "imdb_rating": "8.9",
+    "genres": ["Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0050083_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0050083_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0050083_screenshot3.jpg"]
+}, {
+    "id": 6,
+    "title": "Schindler's List",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0108052_poster.jpg",
+    "year": "1993",
+    "country": "USA",
+    "imdb_rating": "8.9",
+    "genres": ["Biography", "Drama", "History"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0108052_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0108052_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0108052_screenshot3.jpg"]
+}, {
+    "id": 7,
+    "title": "Pulp Fiction",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0110912_poster.jpg",
+    "year": "1994",
+    "country": "USA",
+    "imdb_rating": "8.9",
+    "genres": ["Crime", "Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0110912_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0110912_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0110912_screenshot3.jpg"]
+}, {
+    "id": 8,
+    "title": "The Lord of the Rings: The Return of the King",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0167260_poster.jpg",
+    "year": "2003",
+    "country": "USA, New Zealand",
+    "imdb_rating": "8.9",
+    "genres": ["Adventure", "Drama", "Fantasy"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0167260_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0167260_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0167260_screenshot3.jpg"]
+}, {
+    "id": 9,
+    "title": "The Good, the Bad and the Ugly",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0060196_poster.jpg",
+    "year": "1966",
+    "country": "Italy, Spain, West Germany, USA",
+    "imdb_rating": "8.9",
+    "genres": ["Western"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0060196_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0060196_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0060196_screenshot3.jpg"]
+}, {
+    "id": 10,
+    "title": "Fight Club",
+    "poster": "https:\/\/moviesapi.ir\/images\/tt0137523_poster.jpg",
+    "year": "1999",
+    "country": "USA, Germany",
+    "imdb_rating": "8.8",
+    "genres": ["Drama"],
+    "images": ["https:\/\/moviesapi.ir\/images\/tt0137523_screenshot1.jpg", "https:\/\/moviesapi.ir\/images\/tt0137523_screenshot2.jpg", "https:\/\/moviesapi.ir\/images\/tt0137523_screenshot3.jpg"]
+}],
+    "metadata": {
+    "current_page": "1",
+    "per_page": 10,
+    "page_count": 25,
+    "total_count": 250
+}
+};
+
+    function contentList(data, containerTitle ){
+    let container = document.querySelector(".movieSliderMenu");
+    let menu = document.createElement("div");
+    let contentTitle = document.createElement("h3");
+    contentTitle.innerHTML = containerTitle;
+    menu.classList.add("movieSliderItems");
+    menu.classList.add("col-12");
+    menu.classList.add("d-flex");
+    menu.classList.add("flex-row");
+
+    data.forEach(function (movie){
+    let menuItem = document.createElement("div");
+    menuItem.classList.add("movieSliderItem");
+    menuItem.classList.add("col-2");
+    menuItem.innerHTML = `<a href="#"><img src=${movie.poster} /> <h3>${movie.title}</h3></a>`
+    menu.appendChild(menuItem);
+});
+    container.appendChild(menu);
+    menu.appendChild(contentTitle);
+}
+    contentList(movies1.data, "ویژه");
+    contentList(movies2.data, "هلو");
